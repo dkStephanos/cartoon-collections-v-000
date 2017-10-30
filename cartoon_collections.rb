829c86c5afdc342d1ef5e27d587df6dfdebbd7d7
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 16:15:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,14 @@ def roll_call_dwarves(dwarf_names)
   }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planateers)
+  formatted_planateers = []
+  counter = 0
+
+  while counter < planateers.length {
+    formatted_planateers << planateers[counter].capitalize + "!"
+  }
+
 end
 
 def long_planeteer_calls# code an argument here
